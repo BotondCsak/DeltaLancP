c8dc353fb00b1e696e8e3e41e748ae9cee3a8e00
BotondCsak <botond.csak@me.com>
2025-06-13 18:10:26 +0200

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,4 +1,5 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import {DoubleNumber} from "./components/DoubleNumber";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -7,19 +8,12 @@ export const PLASMIC = initPlasmicLoader({
       token: "xPweHauT3tMu84TmBDAoiKQTUOnIGMyAobx9RjixPtMP0NbopH9FaGPzLEIpKqUhhIHPzzQYdxewirGJkQ",
     },
   ],
-
-  // By default Plasmic will use the last published version of your project.
-  // For development, you can set preview to true, which will use the unpublished
-  // project, allowing you to see your designs without publishing.  Please
-  // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
-// You can register any code components that you want to use here; see
-// https://docs.plasmic.app/learn/code-components-ref/
-// And configure your Plasmic project to use the host url pointing at
-// the /plasmic-host page of your nextjs app (for example,
-// http://localhost:3000/plasmic-host).  See
-// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
-
-// PLASMIC.registerComponent(...);
+PLASMIC.registerComponent(DoubleNumber, {
+  name: "DoubleNumber",
+  displayName: "DoubleNumber",
+  props: {},
+  
+});
